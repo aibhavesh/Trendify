db483161c66d49a677bd749bbd8b84844a20213a
Bhavesh Prajapat <aibhavesh27@gmail.com>
2025-11-24 00:04:47 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+//import loginRoutes from './routes/loginRoutes.js';
+//import userRoutes from './routes/userRoutes.js';
+//import registerRoutes from './routes/registerRoutes.js';
+//import productRoutes from './routes/productRoutes.js';
+//import categoryRoutes from './routes/categoryRoutes.js';
+//import adminRoutes from './routes/adminRoutes.js';
+//import contactRoutes from './routes/contactRoutes.js';
+//import uploadRoutes from './routes/uploadRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+//app.use('/api/login', loginRoutes);
+//app.use('/api/users', userRoutes);
+//app.use('/api/register', registerRoutes);
+//app.use('/api/products', productRoutes);
+//app.use('/api/categories', categoryRoutes);
+//app.use('/api/admin', adminRoutes);
+//app.use('/api/contact', contactRoutes);
+//app.use('/api/upload', uploadRoutes);
+
+
+app.get('/', (req, res)=>{
+    res.send('Server is up and running!');
+});
+
+export default app;
